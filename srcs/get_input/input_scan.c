20ff1eea32bc0ffba7c5ec824a08baefcfb0fb7d
HoangPham <pham.hoang1591@gmail.com>
2020-07-10 12:18:29 +0300

    fix some reading error maps

--- srcs/get_input/input_scan.c
+++ srcs/get_input/input_scan.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   input_scan.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gmolin <gmolin@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: hopham <hopham@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/18 11:27:13 by hopham            #+#    #+#             */
-/*   Updated: 2020/06/17 16:45:21 by gmolin           ###   ########.fr       */
+/*   Updated: 2020/07/10 11:50:01 by hopham           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,70 @@
 
 static void		scanning_links(char **line, t_lem *room)
 {
+	if (*line[0] == '#' && *line[1] != '#')
+	{
+		ft_printf("%s\n", *line);
+		ft_strdel(line);
+		return ;
+	}
 	add_links(room, *line);
 	ft_strdel(line);
 }
 
-static void		scanning_rooms(char **line, t_lem *room)
+static int		scanning_rooms(char **line, t_lem *room, int check)
 {
 	(ft_strstr(*line, "##end") || ft_strstr(*line, "##start"))
 	? ft_printf("%s\n", *line) : 0;
-	if (ft_strcmp("##start", *line) == 0)
+	if (ft_strcmp("##start", *line) == 0 && check == 0)
 	{
 		room->c_start++;
 		free(*line);
 		get_next_line(0, line);
+		if (ft_strlen(*line) == 0)
+			return (0);
 		(room->c_start == 1) ? add_rooms("start", line, room) : 0;
 	}
-	else if (ft_strcmp("##end", *line) == 0)
+	else if (ft_strcmp("##end", *line) == 0 && check == 0)
 	{
 		room->c_end++;
 		free(*line);
 		get_next_line(0, line);
+		if (ft_strlen(*line) == 0)
+			return (0);
 		(room->c_end == 1) ? add_rooms("end", line, room) : 0;
 	}
 	else if (*line[0] == '#' && *line[1] != '#')
 	{
 		ft_printf("%s\n", *line);
 		ft_strdel(line);
-		return ;
+		return (2) ;
 	}
-	else
+	else if (check == 0)
 		add_rooms("room", line, room);
 	ft_strdel(line);
+	return (1);
 }
 
 int				input_scan(t_lem *room)
 {
 	char	*line;
+	int		check;
 
+	check = 0;
 	while (get_next_line(0, &line) > 0)
 	{
 		if (!ft_strstr(line, "-"))
-			scanning_rooms(&line, room);
+		{
+			if (scanning_rooms(&line, room, check) == 0)
+				break ;	
+		}
 		else if (ft_strstr(line, "-"))
+		{
 			scanning_links(&line, room);
+			check = 1;
+		}
+		else if (ft_strlen(line) == 0)
+			break ;
 	}
 	if (room->c_start != 1 || room->c_end != 1)
 		ft_error("ERROR: No start or end room.");
